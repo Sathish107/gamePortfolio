5575f2b85f30140ce916cca9066c1316c29b6fc4
Balasanthosh A <maarimuthu107@gmail.com>
2024-01-27 22:12:46 +0530

    completed initial mobile first design

--- index.js
+++ index.js
@@ -17,32 +17,40 @@ function handleClick(){
 }
 
 
+// function handleBodyClick(){
+//     if(isClicked){
+//         navElement.style.display="none"
+//         menuIcon.classList.remove('fa-x')
+//         isClicked=false
+//     }
+// }
 
-var videos = document.querySelectorAll('video');
 
-var options = {
-    root: null,
-    rootMargin: '0px',
-    threshold: 1
-};
+// var videos = document.querySelectorAll('video');
 
-function handleIntersection(entries, observer) {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            videos.forEach(video => {
-                if (video !== entry.target) {
-                    video.pause();
-                }
-            });
-            entry.target.play();
-        } else {
-            entry.target.pause();
-        }
-    });
-}
+// var options = {
+//     root: null,
+//     rootMargin: '0px',
+//     threshold: 1
+// };
 
-var observer = new IntersectionObserver(handleIntersection, options);
+// function handleIntersection(entries, observer) {
+//     entries.forEach(entry => {
+//         if (entry.isIntersecting) {
+//             videos.forEach(video => {
+//                 if (video !== entry.target) {
+//                     video.pause();
+//                 }
+//             });
+//             entry.target.play();
+//         } else {
+//             entry.target.pause();
+//         }
+//     });
+// }
 
-videos.forEach(video => {
-    observer.observe(video);
-});
+// var observer = new IntersectionObserver(handleIntersection, options);
+
+// videos.forEach(video => {
+//     observer.observe(video);
+// });
